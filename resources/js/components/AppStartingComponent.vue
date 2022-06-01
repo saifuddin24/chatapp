823f4081a6a22c8f0b57adff6dfe53a40edda798
saifuddin24 <saif.bigc@gmail.com>
2022-06-01 12:14:05 +0600

    Environment Setup

---
+++ resources/js/components/AppStartingComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body bg-green-500" :class="mclass">
+                        I'm an example component.<div>{{ message }}</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                message: '',
+                mclass :''
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        created() {
+            Echo.channel('notification')
+                .listen('MessageNotification', (e ,r) => {
+                    console.log( 'MESSAGE: ',e ,r);
+                    this.message = "Message by PUSHER";
+                    this.mclass = ' bg-red-600'
+                });
+        }
+    }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        "./resources/**/*.{html,js,php,blade.php,vue,css}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
